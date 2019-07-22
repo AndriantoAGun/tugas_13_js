bfbf569d4d90c8cacbfe9ba15108602cf17807d1
Andrianto A Gunawan <andrianto22491@gmail.com>
2019-07-22 08:51:45 +0700

    Tugas 13 JavaScript
    tugas Niomic tentang JavaScript meliputi modify Array dengan Unshift

---
+++ tugas_13.js
@@ -0,0 +1,11 @@
+function tampilBuahBuahan() {
+  var buah0 = ['Pisang', 'Jeruk']
+  console.log(buah0);
+
+  var buah1 = buah0.unshift('Apel', 'Mangga')
+  console.log(buah0);
+
+  return tampilBuahBuahan;
+}
+
+tampilBuahBuahan();
